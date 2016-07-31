7f8c7a6baae8a028107f790929dd42f5f60ccc9e
rickzx98 <rickzx98@gmail.com>
2016-07-31 22:51:32 +0800

    updates

---
+++ src/app/core/navigation-links/navigation-links.component.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    angular.module('app.core')
+        .component('navigationLinks', {
+            templateUrl: 'src/app/core/navigation-links/navigation-links.html'
+        });
+})();
