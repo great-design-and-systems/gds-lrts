92df9b351f96e28587f350eecfcedbce676663bd
rickzx98 <rickzx98@gmail.com>
2016-08-05 03:10:34 +0800

    updates

--- src/exporter/events/add-export-items-csv-event.service.js
+++ src/exporter/events/add-export-items-csv-event.service.js
@@ -2,11 +2,24 @@
     'use strict';
     angular.module('app.exporter')
         .run(AddExportItemsCsvEventService);
-    AddExportItemsCsvEventService.$inject = ['$rootScope', 'ExporterResourceService', 'ExporterEvents'];
+    AddExportItemsCsvEventService.$inject = ['$rootScope', 'ExporterResourceService', 'ExporterEvents', 'vendors'];
 
-    function AddExportItemsCsvEventService($rootScope, ExporterResourceService, ExporterEvents) {
+    function AddExportItemsCsvEventService($rootScope, ExporterResourceService, ExporterEvents, vendors) {
         $rootScope.$on(ExporterEvents.ADD_EXPORT_ITEMS_CSV, function ($event, data, callback) {
-            ExporterResourceService.addExportItemsCSV(data.exportId, data.items, function (err, result) {
+            var csvItems = [];
+            for (var i = 0; i < data.items.length; i++) {
+                var item = data.items[i];
+                csvItems.push({
+                    'Time': vendors.moment(item.when).format('LTS'),
+                    'Full name': item.fullname,
+                    'Purpose':item.purpose,
+                    'Department': item.department,
+                    'Person type': item.personType,
+                    'ID': item.personId,
+                    'Date': vendors.moment(item.when).format('MMM Do YY')
+                });
+            }
+            ExporterResourceService.addExportItemsCSV(data.exportId, csvItems, function (err, result) {
                 if (err) {
                     callback(err);
                 } else {
