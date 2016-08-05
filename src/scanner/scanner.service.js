88c9377fc0090148e777c8b4ee7c345531b1c4b4
rickzx98 <rickzx98@gmail.com>
2016-08-05 11:04:51 +0800

    updates

--- src/scanner/scanner.service.js
+++ src/scanner/scanner.service.js
@@ -5,10 +5,8 @@
     ScannerService.$inject = ['vendors', 'API_HOST', 'EventEmitterService', 'MonitorEvents'];
     function ScannerService(vendors, API_HOST, EventEmitterService, MonitorEvents) {
         var socket = vendors.Socket.connect(API_HOST);
-        socket.on('scanned', function (info) {
-            if (info && info.timeInID) {
-                EventEmitterService.emit(MonitorEvents.REFRESH_TODAY_RECORDS_EVENT);
-            }
+        socket.on('scanned', function () {
+            EventEmitterService.emit(MonitorEvents.REFRESH_TODAY_RECORDS_EVENT);
         });
         console.log('Scanner has been started: ', socket);
     }
