ddb429c5acef95f8d889a36bd0b64e1d1e4487e6
rickzx98 <rickzx98@gmail.com>
2016-08-20 04:29:46 +0800

    updates

--- src/labels/events/cascade-labels.event.js
+++ src/labels/events/cascade-labels.event.js
@@ -1,10 +1,14 @@
-(function () {
+(function() {
     angular.module('app.labels')
         .run(Event);
     Event.$inject = ['$rootScope', 'EventEmitterService', 'LabelsEvents'];
+
     function Event($rootScope, EventEmitterService, LabelsEvents) {
-        $rootScope.$on(LabelsEvents.CASCADE_LABELS, function ($event, data, callback) {
+        $rootScope.$on(LabelsEvents.CASCADE_LABELS, function($event, data, callback) {
             callback(undefined, data);
+        });
+        EventEmitterService.onComplete(LabelsEvents.GET_LABELS, function(result) {
+            EventEmitterService.emit(LabelsEvents.CASCADE_LABELS, result);
         });
     }
 })();
